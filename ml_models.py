107c181c723ff66d6ff8ee1eeb5b7c40244c230e
dan <rigoli.dan@gmail.com>
2017-01-12 17:45:47 -0500

    linear and polynomial regression functions

--- ml_models.py
+++ ml_models.py
@@ -103,8 +103,43 @@ class ProjectData(object):
 client_data = [[5,17,15],[4,32,22],[8,3,12]]
 clients = np.transpose(client_data)
 
-sleep = [5,6,7,8,10]
-scores = [65,51,75,75,86]
+def computeLinearRegression(sleep,scores):
+    #	First, compute the average amount of each list
+    avg_sleep = np.average(sleep)
+    avg_scores = np.average(scores)
+
+    #	Then normalize the lists by subtracting the mean value from each entry
+    normalized_sleep = [i-avg_sleep for i in sleep]
+    normalized_scores = [i-avg_scores for i in scores]
+#     normalized_sleep = ['{:.2f}'.format(i-avg_sleep) for i in sleep]
+#     normalized_scores = ['{:.2f}'.format(i-avg_scores) for i in scores]
+    print(normalized_sleep)
+    print(normalized_scores)
+
+    #	Compute the slope of the line by taking the sum over each student
+    #	of the product of their normalized sleep times their normalized test score.
+    #	Then divide this by the sum of squares of the normalized sleep times.
+    slope = sum([x*y for x,y in zip(normalized_sleep,normalized_scores)]) / sum([np.square(y) for y in normalized_sleep])# = 0
+    print(slope)
+    #	Finally, We have a linear function of the form
+    #	y - avg_y = slope * ( x - avg_x )
+    #	Rewrite this function in the form
+    #	y = m * x + b
+    #	Then return the values m, b
+    m = slope
+    b = -slope*avg_sleep + avg_scores
+    #y = m * x + b
+    return m,b
+
+def computePolynomialregression():
+    # polynomial regression
+    #y = p[0] * x**2 + p[1] * x + p[2]
+    pass
+
+#if __name__=="__main__":
+def printLinearRegressionModel():
+    m,b = compute_regression(sleep,scores)
+    print("Your linear model is y={}*x+{}".format(m,b))
 
 class Model(object):
     ''' base model object '''
