035f715102b0416e4052e615edc33cf98287b6bb
dan <rigoli.dan@gmail.com>
2017-05-22 14:14:27 -0400

    update to lists.py

--- lists.py
+++ lists.py
@@ -23,8 +23,30 @@ def loadStudentData(file):
     return pd.read_csv(file)
 
 # === test functions ===
+def testFirstN(count=1000000):
+    return(sum(firstn(count)))
 
 # === model object ===
+
+class firstn(object):
+    def __init__(self, n):
+        self.n = n
+        self.num, self.nums = 0, []
+
+    def __iter__(self):
+        return self
+
+    # Python 3 compatibility
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self.num < self.n:
+            cur, self.num = self.num, self.num+1
+            return cur
+        else:
+            raise StopIteration()
+
 
 class ProjectData(object):
     ''' get and setup data '''
