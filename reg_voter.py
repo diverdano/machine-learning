1f43bdc5963d3996524dfe90a05ab010095247fe
diverdano <diverdano@gmail.com>
2018-02-16 13:25:46 -0500

    voter stuff

--- reg_voter.py
+++ reg_voter.py
@@ -39,7 +39,7 @@ class RegVoters(object):
         "sanbourn+ct",
         "stonehaven+way",
         "torreyanna+cir"]
-    urls_sets       = []
+    url_sets        = []
     test_urls       = []
     test_results    = []
     pages           = {}
@@ -47,8 +47,10 @@ class RegVoters(object):
     def __init__(self):
         self.testURLs()
         self.setupURLs()
+        self.getVoters()
     def testURLs(self):
         '''iterate streets, creating test urls, validate http status'''
+        print('scraping pages and people counts')
         for street in self.streets:
             url = self.domain+street+self.cityst+"/1"
             self.test_urls.append(url)
@@ -56,26 +58,33 @@ class RegVoters(object):
             status      = response.status_code
             char_loc    = response.text.find('Page 1 of ')                        # find pages
             people_loc  = response.text.find(' people who live')
-            if char_loc == -1   : pages = '1'
+            if char_loc == -1   : pages = '1'       # keep string value, used in url
             else                : pages = response.text[char_loc:char_loc + 12].split('<')[0].split(' of ')[1]    # propably better way to do this
-            people_count = response.text[people_loc - 10:people_loc].split(' are ')[1]
+            people_count = int(response.text[people_loc - 10:people_loc].split(' are ')[1])
             self.pages[street] = {'pages':pages, 'people':people_count}
             print("\tstatus: {0} | {1} | {2} | {3}".format(str(status), pages, people_count, street))
         return self.pages
     def setupURLs(self):
         '''iterate streets, creating urls'''
+        print('setting up urls for each street')
         for street in self.streets:
-            print(street)
-            pages   = list(range(1,int(self.pages[street]['pages'])+1)) # assumes 9 pages of names for each street
-            self.urls_sets.append([self.domain+street+self.cityst+str(page) for page in pages])
+            if self.pages[street]['people']>0 :
+                included=True
+                pages   = list(range(1,int(self.pages[street]['pages'])+1)) # assumes 9 pages of names for each street
+                self.url_sets.append([self.domain+street+self.cityst+str(page) for page in pages])
+            else: included=False
+            inc_skip = 'included' if included == True else '--skipped--'
+            print('\t{}'.format(street).ljust(17) + inc_skip)
     def getVoters(self):
-        self.people = []
-        for i, urls in enumerate(self.urls_sets):
-            for url in urls:
-                print(url)
-                response        = util.requests.get(url)
-                self.people.append(util_data.pd.read_html(response.content))      # currently page contains summary table and list of voters
-                # voters          = people[1].rename(columns=lambda x: x.strip()) # drop the summary table and strip space in column names
-                # voters.drop(voters[[3]], axis=1, inplace=True)
-                # self.people.append(voters)
-#        self.people = util_data.pd.concat([self.people[item] for item in list(range(len(self.people)))])
+        print('scraping people from web pages')
+        for i, urls in enumerate(self.url_sets):
+            print('\n\t' + self.streets[i-1])
+            for j, url in enumerate(urls):
+                print(str(j), sep=' ', end='', flush=True)
+                self.people.append(util_data.pd.read_html(util.requests.get(url).content))
+    def cleansVoters(self):
+        '''apply data scrubbing to scraped voter records'''
+        self.voters = util_data.pd.concat([self.people[item][1] for item in list(range(len(self.people)))]) # drop summary tables and concat list of dfs to one df
+        self.voters.rename(columns=lambda x: x.strip(), inplace=True)         # strip spaces in column names
+        self.voters.drop(self.voters[[3]], axis=1, inplace=True)   # drop extra column
+#        self.voters['name'], self.voters['age'] = zip(*voters.voters['Person'].str.split("  ", expand=True).tolist())
